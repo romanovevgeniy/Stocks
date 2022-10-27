cbdf9b7ab59a209477e5bebf0f2976fe9f90da18
eromanov <romanov.evgeniy86@gmail.com>
2022-10-27 20:19:31 +0600

    Инициализация проекта

---
+++ Stocks-demo/Controllers/StocksDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StocksDetailViewController.swift
+//  Stocks-demo
+//
+//  Created by Евгений Романов on 27.10.2022.
+//
+
+import UIKit
+
+class StocksDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
