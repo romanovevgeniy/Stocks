0ef53c8d49221a8bec00079972c12e414da73716
eromanov <romanov.evgeniy86@gmail.com>
2022-10-30 00:03:30 +0500

    Добавление контроллера поиска

--- Stocks-demo/Controllers/WatchListViewController.swift
+++ Stocks-demo/Controllers/WatchListViewController.swift
@@ -12,6 +12,31 @@ class WatchListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setUpSearchController()
+    }
+    
+    private func setUpSearchController() {
+        let resultVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
+}
+
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+        let resultVC = searchController.searchResultsController as? SearchResultsViewController,
+        !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        // оптимизация по количеству запросов
+        
+        // вызов API для поиска
+        
+        // обновление контроллера результатов
+        
     }
 }
 
