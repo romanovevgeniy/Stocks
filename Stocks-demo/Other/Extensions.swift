0a75de92f6b6713cbab97edb00401229048b0b10
eromanov <romanov.evgeniy86@gmail.com>
2022-11-10 00:44:09 +0600

    Создание расширений для форматирования даты

--- Stocks-demo/Other/Extensions.swift
+++ Stocks-demo/Other/Extensions.swift
@@ -8,12 +8,25 @@
 import Foundation
 import UIKit
 
+extension String {
+    static func string(from timeInterval: TimeInterval) -> String {
+        let date = Date(timeIntervalSince1970: timeInterval)
+        return DateFormatter.prettyDateFormatter.string(from: date)
+    }
+}
+
 //MARK: DateFormatter
 
 extension DateFormatter {
     static let newsDateFormatter: DateFormatter = {
        let formatter = DateFormatter()
         formatter.dateFormat = "YYYY-MM-dd"
+        return formatter
+    }()
+    
+    static let prettyDateFormatter: DateFormatter = {
+       let formatter = DateFormatter()
+        formatter.dateStyle = .medium
         return formatter
     }()
 }
